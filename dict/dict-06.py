18e506ae320f762e2b60fe491d4915f81b43e168
ommd3076 <ommd3076@gmail.com>
2026-08-27 12:05:37 +0530

    commit 1a2b3c4d5e6f7g8h9i0j

--- dict/dict-06.py
+++ dict/dict-06.py
@@ -1,1 +1,9 @@
 status_codes = [200, 404, 200, 500, 200, 404]
+counts = {}
+for n in status_codes:
+    if n in counts:
+        counts[n] += 1
+    else:
+        counts[n] = 1
+        
+print(counts)
