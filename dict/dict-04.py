50485bdd3465d8835443864e90be4cd324587058
ommd3076 <ommd3076@gmail.com>
2026-09-15 22:40:01 +0530

    21esfq13

--- dict/dict-04.py
+++ dict/dict-04.py
@@ -1,3 +1,4 @@
+# This program creates a list of active server IDs from a list of server dictionaries.
 servers = [{"id": 1, "state": "up"}, {"id": 2, "state": "down"}, {"id": 3, "state": "up"}]
 active_ids = []
 for s in servers:
